2ad45d2d1cbb00fb168e31e5ef4cd0fa47db218d
VasileiosBouzas <bouzasbasilis@hotmail.com>
2019-12-23 17:30:28 +0100

    folder structure

--- geometry/point.py
+++ geometry/point.py
@@ -1,9 +1,13 @@
-from abc import ABC, abstractmethod, abstractproperty
-from exceptions import (InvalidGeometry,
-                        InvalidOperation)
+from abc import (ABC,
+                 abstractmethod,
+                 abstractproperty)
+from utils.exceptions import (InvalidGeometry,
+                              InvalidOperation)
 
 
 class Point(ABC):
+    '''Point Class Base Implementation'''
+
     def __init__(self, *coords, dim):
         if len(coords) not in [0, dim]:
             raise InvalidGeometry(
@@ -14,10 +18,6 @@ class Point(ABC):
                not coord == None:
                 raise InvalidGeometry("Insert only numeric values or none")
         self.dim = dim
-
-    @abstractproperty
-    def coords(self):
-        pass
 
     def __str__(self):
         values = [str(v) for v in self.coords]
@@ -45,6 +45,12 @@ class Point(ABC):
         if self.dim != other.dim:
             raise InvalidOperation("The point dimensions are not equal")
 
+    '''Overrides'''
+
+    @abstractproperty
+    def coords(self):
+        pass
+
     @abstractmethod
     def __add__(self, other):
         self.check_operation(other)
@@ -57,13 +63,21 @@ class Point(ABC):
 
     @abstractmethod
     def translate(self, *translation):
+        '''Translate point'''
         if len(translation) != self.dim:
             raise InvalidOperation("Invalid translation")
         return [sc + tc for sc, tc in zip(self.coords, translation)]
 
 
 class Point2D(Point):
+    '''2D Point Implemenation'''
+
     def __init__(self, *coords):
+        '''
+        Initialize 2D point
+        i.e. p = Point2D(1, 2)
+        Access point coordinates by p.x, p.y
+        '''
         super().__init__(*coords, dim=2)
         x, y = None, None
         if len(coords) == self.dim:
@@ -76,20 +90,28 @@ class Point2D(Point):
         return self.x, self.y
 
     def __add__(self, other):
-        return Point2D(*self.add(other))
+        return Point2D(*super().__add__(other))
 
     def __sub__(self, other):
-        return Point2D(*self.sub(other))
+        return Point2D(*super().__sub__(other))
 
     def translate(self, *translation):
         return Point2D(*super().translate(*translation))
 
     def to3D(self):
+        '''Convert to 3D point'''
         return Point3D(self.x, self.y, 0)
 
 
 class Point3D(Point):
+    '''3D Point Implementation'''
+
     def __init__(self, *coords):
+        '''
+        Initialize 2D point
+        i.e. p = Point3D(1, 2, 3)
+        Access point coordinates by p.x, p.y, p.z
+        '''
         super().__init__(*coords, dim=3)
         x, y, z = None, None, None
         if len(coords) == self.dim:
@@ -112,4 +134,5 @@ class Point3D(Point):
         return Point3D(*super().translate(*translation))
 
     def to2D(self):
+        '''Convert to 2D point'''
         return Point2D(self.x, self.y)
